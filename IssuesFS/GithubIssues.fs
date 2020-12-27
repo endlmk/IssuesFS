2bdbe71443534535957d174e4043d34f61bb5a8a
endlmk <1269467+endlmk@users.noreply.github.com>
2020-12-27 23:18:03 +0900

    Impl GithubIssues.

---
+++ IssuesFS/GithubIssues.fs
@@ -0,0 +1,20 @@
+﻿namespace ISsuesFS
+module GithubIssues =
+
+    open FSharp.Data
+
+    let userAgent = [ "User-agent", "Elixir dave@pragprog.com" ]
+
+    let issuesUrl user project =
+        $"https://api.github.com/repos/{user}/{project}/issues"
+    
+    let handleResponse (response : HttpResponse) = 
+        match response.StatusCode with
+        | 200 -> Ok response.Body
+        | _ -> Error response.Body
+    
+    let fetch user project =
+        let request url = Http.Request(url, headers = userAgent)
+        issuesUrl user project
+        |> request
+        |> handleResponse
